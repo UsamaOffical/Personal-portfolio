a6a953fd64e8bc036892f13b2fb6a302c1465275
UsamaOffical <usamasheikh786@gmail.com>
2025-03-25 07:01:03 +0500

    again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import Image from "next/image";
+
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div>
-      {/* main bg #1c2541
-      <p>second bg #3a506b</p>
-      <p>color for text #489fb5</p>
-      <p>color for text complete #f6f6f6</p> */}
       <Navbar/>
       <Hero/>
     </div>
